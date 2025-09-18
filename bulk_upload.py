40bd4d72016f91f41cc384bd7308f8cc49c9706b
thewillyP <williamsleepark@gmail.com>
2025-09-18 13:01:41 -0400

    for some reason it removed my slurm config

--- bulk_upload.py
+++ bulk_upload.py
@@ -119,6 +119,22 @@ def bulk_upload_hdf5_files_api(offline_log_dir: str, max_workers: int, batch_siz
 if __name__ == "__main__":
     task: Task = Task.init(project_name="utilities", task_name="bulk_upload_hdf5_metrics_api")
 
+    # Slurm configuration
+    slurm_params = {
+        "memory": "16GB",
+        "time": "06:00:00",
+        "cpu": 4,
+        "gpu": 0,
+        "log_dir": "/vast/wlp9800/logs",
+        "singularity_overlay": "",
+        "singularity_binds": "/scratch/wlp9800/clearml:/scratch",
+        "container_source": {"sif_path": "/scratch/wlp9800/images/devenv-cpu.sif", "type": "sif_path"},
+        "use_singularity": True,
+        "setup_commands": "module load python/intel/3.8.6",
+        "skip_python_env_install": True,
+    }
+    task.connect(slurm_params, name="slurm")
+
     # Upload configuration for API version
     upload_config = {
         "offline_log_dir": "/scratch/clearml/offline_logs",

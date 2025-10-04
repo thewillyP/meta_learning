eeb46febd5ab923c459b70f6232d589a23d0d666
thewillyP <williamsleepark@gmail.com>
2025-10-04 06:09:19 -0400

    prep for exp

--- src/main_clearml.py
+++ src/main_clearml.py
@@ -19,7 +19,7 @@ from meta_learn_lib.logger import ClearMLLogger, HDF5Logger, MatplotlibLogger, M
 
 def main():
     _jitter_rng = random.Random()
-    # time.sleep(_jitter_rng.uniform(1, 60))
+    time.sleep(_jitter_rng.uniform(1, 60))
 
     # names don't matter, can change in UI
     # clearml.Task.set_offline(True)
@@ -46,7 +46,7 @@ def main():
     task.connect(unstructure(slurm_params), name="slurm")
 
     config = GodConfig(
-        clearml_run=True,
+        clearml_run=False,
         data_root_dir="/scratch/datasets",
         log_dir="/scratch/offline_logs",
         # dataset=CIFAR10Config(3072),

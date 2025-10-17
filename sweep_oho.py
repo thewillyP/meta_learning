2330d779155e962d29c077bd764b7826a4609ded
thewillyP <williamsleepark@gmail.com>
2025-10-17 17:55:49 -0400

    exp failed

--- sweep_oho.py
+++ sweep_oho.py
@@ -26,7 +26,7 @@ optimizer = HyperParameterOptimizer(
         # OHO
         DiscreteParameterRange("config/learners/1/learner/_type", values=["RTRLFiniteHvpConfig"]),
         DiscreteParameterRange("config/learners/1/learner/epsilon", values=[1.0e-3]),
-        DiscreteParameterRange("config/learners/1/optimizer/learning_rate/value", values=[1e-3]),
+        DiscreteParameterRange("config/learners/1/optimizer/learning_rate/value", values=[5e-3, 2e-3, 1e-4]),
         # DiscreteParameterRange("config/learners/1/optimizer/momentum", values=[0.9]),
         DiscreteParameterRange(
             "config/learners/0/optimizer/learning_rate/value",

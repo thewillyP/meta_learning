3026f3f151033afc1ba602d110e76709cb1af080
thewillyP <williamsleepark@gmail.com>
2025-11-12 16:58:05 -0500

    oho

--- sweep_oho.py
+++ sweep_oho.py
@@ -4,7 +4,7 @@ from clearml import Task
 # Create optimizer task
 opt_task = Task.init(
     project_name="oho",
-    task_name="Seed+ILR Sweep: Batch-5000,Epochs-1000,FashionMNIST,MLP,SGD-ADAM,BPTT-RTRL",
+    task_name="Seed+ILR Sweep: Batch-4000,Epochs-1000,CIFAR10,RNN-512,SGD-ADAM,BPTT-RTRL",
 )
 # task_name="Fixed Seed+ILR Sweep: Batch-2,Epochs-20,FashionMNIST,MLP,SGD-Adam,BPTT-ID"
 # task_name="OHO Seed+ILR Sweep: Batch-2,Epochs-20,FashionMNIST,MLP,SGD/SGDN-Adam,BPTT-RTRL"
@@ -12,7 +12,7 @@ opt_task.execute_remotely(queue_name="services", clone=False, exit_process=True)
 
 # Configure optimizer
 optimizer = HyperParameterOptimizer(
-    base_task_id="0d93867652924ea2ae6cdb094eb59afe",  # Use the actual task ID
+    base_task_id="81048c3d4eaa4eaab73fac2783754881",  # Use the actual task ID
     hyper_parameters=[
         # Seed configurations as complete seed objects
         DiscreteParameterRange(
@@ -40,8 +40,8 @@ optimizer = HyperParameterOptimizer(
         # Fixed parameters
         DiscreteParameterRange("config/clearml_run", values=[True]),
         DiscreteParameterRange("config/num_base_epochs", values=[1000]),
-        DiscreteParameterRange("config/data/0/num_examples_in_minibatch", values=[5000]),
-        DiscreteParameterRange("config/data/1/num_examples_in_minibatch", values=[5000]),
+        DiscreteParameterRange("config/data/0/num_examples_in_minibatch", values=[4000]),
+        DiscreteParameterRange("config/data/1/num_examples_in_minibatch", values=[4000]),
         # DiscreteParameterRange("config/data/0/train_percent", values=[80.00]),
         # DiscreteParameterRange("config/data/1/train_percent", values=[20.00]),
         # DiscreteParameterRange("config/data/0/num_steps_in_timeseries", values=[28]),
@@ -55,9 +55,9 @@ optimizer = HyperParameterOptimizer(
         ),
         DiscreteParameterRange("config/data_root_dir", values=["/scratch/datasets"]),
         # Slurm configurations
-        DiscreteParameterRange("slurm/time", values=["01:00:00"]),
-        DiscreteParameterRange("slurm/cpu", values=[1]),
-        DiscreteParameterRange("slurm/memory", values=["14GB"]),
+        DiscreteParameterRange("slurm/time", values=["02:00:00"]),
+        DiscreteParameterRange("slurm/cpu", values=[2]),
+        DiscreteParameterRange("slurm/memory", values=["16GB"]),
         DiscreteParameterRange("slurm/use_singularity", values=[True]),
         DiscreteParameterRange("slurm/skip_python_env_install", values=[True]),
         # gpu

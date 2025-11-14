f1a0efee051536e65185dc9dc815f017ef6f3687
thewillyP <williamsleepark@gmail.com>
2025-11-14 03:10:32 -0500

    make smoother?

--- sweep_oho.py
+++ sweep_oho.py
@@ -27,7 +27,7 @@ optimizer = HyperParameterOptimizer(
         # OHO
         DiscreteParameterRange("config/learners/1/learner/_type", values=["RTRLFiniteHvpConfig"]),
         DiscreteParameterRange("config/learners/1/learner/epsilon", values=[1e-3]),
-        DiscreteParameterRange("config/learners/1/learner/momentum1", values=[0.9]),
+        DiscreteParameterRange("config/learners/1/learner/momentum1", values=[0.95]),
         DiscreteParameterRange("config/learners/1/optimizer/learning_rate/value", values=[1e-3]),
         DiscreteParameterRange(
             "config/learners/0/optimizer/learning_rate/value",

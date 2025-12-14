2eb28756d79777a717a225bb6b05a5aa03b2c40c
thewillyP <williamsleepark@gmail.com>
2025-12-13 21:14:13 -0500

    lets go

--- sweep_oho.py
+++ sweep_oho.py
@@ -4,14 +4,14 @@ from clearml import Task
 # Create optimizer task
 opt_task = Task.init(
     project_name="oho",
-    task_name="Eigenvalue Demo: Batch-5000,Epochs-1000,FashionMNIST,MLP-3-128,SGD-SGD,BPTT-RTRL",
+    task_name="OHO Seed+ILR Sweep: Batch-4000,Epochs-1000,CIFAR10,GRU-128,SGD-SGD,BPTT-RTRL",
     task_type=Task.TaskTypes.optimizer,
 )
 # task_name="Fixed Seed+ILR Sweep: Batch-2,Epochs-20,FashionMNIST,MLP,SGD-Adam,BPTT-ID"
 # task_name="OHO Seed+ILR Sweep: Batch-2,Epochs-20,FashionMNIST,MLP,SGD/SGDN-Adam,BPTT-RTRL"
 opt_task.execute_remotely(queue_name="services", clone=False, exit_process=True)
 
-task = Task.get_task(project_name="oho", task_name="mlp_3-128_test")
+task = Task.get_task(project_name="oho", task_name="gru128_exp")
 
 # Configure optimizer
 optimizer = HyperParameterOptimizer(
@@ -20,15 +20,15 @@ optimizer = HyperParameterOptimizer(
         # Seed configurations as complete seed objects
         DiscreteParameterRange(
             "config/seed/global_seed",
-            values=[1, 2],
+            values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
         ),
         DiscreteParameterRange("config/seed/test_seed", values=[12345]),
         # dataset
         # DiscreteParameterRange("config/dataset/_type", values=["FashionMnistConfig"]),
         # DiscreteParameterRange("config/dataset/n_in", values=[28]),
         # OHO
-        DiscreteParameterRange("config/learners/1/learner/_type", values=["RTRLHessianDecompConfig"]),
-        DiscreteParameterRange("config/learners/1/learner/epsilon", values=[1e-4]),
+        DiscreteParameterRange("config/learners/1/learner/_type", values=["RTRLFiniteHvpConfig"]),
+        DiscreteParameterRange("config/learners/1/learner/epsilon", values=[1e-3]),
         DiscreteParameterRange("config/learners/1/learner/momentum1", values=[0.9]),
         DiscreteParameterRange(
             "config/learners/0/optimizer/learning_rate/value",
@@ -42,8 +42,8 @@ optimizer = HyperParameterOptimizer(
         # Fixed parameters
         DiscreteParameterRange("config/clearml_run", values=[True]),
         DiscreteParameterRange("config/num_base_epochs", values=[1000]),
-        DiscreteParameterRange("config/data/0/num_examples_in_minibatch", values=[5000]),
-        DiscreteParameterRange("config/data/1/num_examples_in_minibatch", values=[5000]),
+        DiscreteParameterRange("config/data/0/num_examples_in_minibatch", values=[4000]),
+        DiscreteParameterRange("config/data/1/num_examples_in_minibatch", values=[4000]),
         # DiscreteParameterRange("config/data/0/train_percent", values=[80.00]),
         # DiscreteParameterRange("config/data/1/train_percent", values=[20.00]),
         # DiscreteParameterRange("config/data/0/num_steps_in_timeseries", values=[28]),
@@ -57,7 +57,7 @@ optimizer = HyperParameterOptimizer(
         ),
         DiscreteParameterRange("config/data_root_dir", values=["/scratch/datasets"]),
         # Slurm configurations
-        DiscreteParameterRange("slurm/time", values=["04:00:00"]),
+        DiscreteParameterRange("slurm/time", values=["02:00:00"]),
         DiscreteParameterRange("slurm/cpu", values=[2]),
         DiscreteParameterRange("slurm/memory", values=["16GB"]),
         DiscreteParameterRange("slurm/use_singularity", values=[True]),

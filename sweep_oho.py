d487b0abd0c6158e16e20e1ee38dfc859ddc0608
thewillyP <williamsleepark@gmail.com>
2026-03-17 22:17:12 -0400

    full sweep do it

--- sweep_oho.py
+++ sweep_oho.py
@@ -16,7 +16,7 @@ optimizer = HyperParameterOptimizer(
         # Sweep
         DiscreteParameterRange(
             "config/seed/global_seed",
-            values=[1],
+            values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
         ),
         # Inner optimizer hyperparameters
         DiscreteParameterRange("config/hyperparameters/meta1_sgd1_lr/value", values=[1.0e-3]),

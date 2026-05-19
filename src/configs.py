8b7e4e80fe04d8cb945b1f4a09d331ac0c74e85c
thewillyP <williamsleepark@gmail.com>
2026-05-19 18:29:03 -0400

    update

--- src/configs.py
+++ src/configs.py
@@ -8561,7 +8561,7 @@ OHO_RNN32_TEST = GodConfig(
 if __name__ == "__main__":
     for name, config in [
         # ("SOS_BETA_OHO", SOS_BETA_OHO),
-        # ("SOS_BETA_OHO_2CONV", SOS_BETA_OHO_2CONV),
+        ("SOS_BETA_OHO_2CONV", SOS_BETA_OHO_2CONV),
         # ("VAE_BASELINE", VAE_BASELINE),
         # ("VAE_BASELINE_ADAM", VAE_BASELINE_ADAM),
         # ("VAE_BASELINE_MLP", VAE_BASELINE_MLP),

fed83c828ef4d95fdcd16b139f560bbccfbd7a92
thewillyP <williamsleepark@gmail.com>
2026-06-08 02:35:36 -0400

    reparametrization

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,6 +2,11 @@ import os
 import jax
 
 jax.config.update("jax_enable_x64", True)
-jax.config.update("jax_compilation_cache_dir", os.path.expanduser("~/.cache/jax"))
-jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
-jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+
+cache_dir = os.environ.get("JAX_COMPILATION_CACHE_DIR", os.path.expanduser("~/.cache/jax"))
+if cache_dir.lower() in ("", "none", "off", "disable"):
+    jax.config.update("jax_compilation_cache_dir", None)
+else:
+    jax.config.update("jax_compilation_cache_dir", cache_dir)
+    jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+    jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)

2ea5a426946233b4715ecc8d00c539bec0099207
thewillyP <williamsleepark@gmail.com>
2026-08-17 18:58:15 -0400

    separate param and state creation from each other

--- src/meta_learn_lib/construct/init.py
+++ src/meta_learn_lib/construct/init.py
@@ -19,134 +19,275 @@ from meta_learn_lib.construct.term import (
     Term,
     Validator,
 )
-from meta_learn_lib.lib_types import ArrayTree, PRNG
+from meta_learn_lib.lib_types import ArrayTree, JACOBIAN, PRNG
 
-from typing import cast, overload
+from typing import Callable, cast, overload
 import equinox as eqx
 import jax
+import jax.flatten_util
 import jax.numpy as jnp
 import optax
 from plum import dispatch
 
 
 @overload
-def val_init[S, X, Y, HP, P](v: SameModel[S, X, Y, HP, P], s: S, ctx: int, key: PRNG) -> tuple[tuple[Unit, Unit], S]:
-    return ((Unit(), Unit()), s)
+def val_port[S, X, Y, HP, P](v: SameModel[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[Unit, Unit]:
+    return (Unit(), Unit())
 
 
 @overload
-def val_init[S, X, Y, HP, P, SV, XV, HPV, PV](
-    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], s: S, ctx: int, key: PRNG
-) -> tuple[tuple[HPV, PV], SV]:
+def val_port[S, X, Y, HP, P, SV, XV, HPV, PV](
+    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], ctx: int, key: PRNG
+) -> tuple[HPV, PV]:
     raise NotImplementedError
 
 
 @dispatch
-def val_init[S, X, Y, HP, P, SV, XV, HPV, PV](
-    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], s: S, ctx: int, key: PRNG
-) -> tuple[tuple[HPV, PV], SV]:
+def val_port[S, X, Y, HP, P, SV, XV, HPV, PV](
+    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], ctx: int, key: PRNG
+) -> tuple[HPV, PV]:
     raise NotImplementedError
 
 
 @overload
-def init(t: Linear, ctx: int, key: PRNG) -> tuple[tuple[Unit, eqx.nn.Linear], Unit]:
+def port(t: Linear, ctx: int, key: PRNG) -> tuple[Unit, eqx.nn.Linear]:
     k_layer, k_w = jax.random.split(key)
     layer = eqx.nn.Linear(ctx, t.n, use_bias=False, key=k_layer)
-    w = initializer(t.init)(k_w, (t.n, ctx))
-    return ((Unit(), eqx.tree_at(lambda l: l.weight, layer, w)), Unit())
+    return (Unit(), eqx.tree_at(lambda l: l.weight, layer, initializer(t.init)(k_w, (t.n, ctx))))
 
 
 @overload
-def init(t: Bias, ctx: int, key: PRNG) -> tuple[tuple[Unit, jax.Array], Unit]:
-    return ((Unit(), initializer(t.init)(key, (ctx,))), Unit())
+def port(t: Bias, ctx: int, key: PRNG) -> tuple[Unit, jax.Array]:
+    return (Unit(), initializer(t.init)(key, (ctx,)))
 
 
 @overload
-def init[HPA, PA](t: Rnn[HPA, PA], ctx: int, key: PRNG) -> tuple[tuple[HPA, tuple[PA, eqx.nn.Linear]], jax.Array]:
-    k_layer, k_rec, k_in, k_h = jax.random.split(key, 4)
+def port[HPA, PA](t: Rnn[HPA, PA], ctx: int, key: PRNG) -> tuple[HPA, tuple[PA, eqx.nn.Linear]]:
+    k_layer, k_rec, k_in = jax.random.split(key, 3)
     layer = eqx.nn.Linear(t.n + ctx, t.n, use_bias=False, key=k_layer)
     w_rec = initializer(Orthogonal())(k_rec, (t.n, t.n))
     w_in = initializer(t.init)(k_in, (t.n, ctx))
-    w = jnp.hstack([w_rec, w_in])
     hp, p = knob(t.alpha)
-    return ((hp, (p, eqx.tree_at(lambda l: l.weight, layer, w))), initializer(t.h0)(k_h, (t.n,)))
+    return (hp, (p, eqx.tree_at(lambda l: l.weight, layer, jnp.hstack([w_rec, w_in]))))
 
 
 @overload
-def init(t: Activation, ctx: int, key: PRNG) -> tuple[tuple[Unit, Unit], Unit]:
-    return ((Unit(), Unit()), Unit())
+def port(t: Activation, ctx: int, key: PRNG) -> tuple[Unit, Unit]:
+    return (Unit(), Unit())
 
 
 @overload
-def init(t: Loss, ctx: int, key: PRNG) -> tuple[tuple[Unit, Unit], Unit]:
-    return ((Unit(), Unit()), Unit())
+def port(t: Loss, ctx: int, key: PRNG) -> tuple[Unit, Unit]:
+    return (Unit(), Unit())
 
 
 @overload
-def init[P](t: Sgd[P], ctx: P, key: PRNG) -> tuple[tuple[Unit, jax.Array], ArrayTree]:
-    return ((Unit(), jnp.asarray(t.lr)), optax.sgd(t.lr).init(cast(optax.Params, ctx)))
+def port[P](t: Sgd[P], ctx: int, key: PRNG) -> tuple[Unit, jax.Array]:
+    return (Unit(), jnp.asarray(t.lr))
 
 
 @overload
-def init[S1, S2, X, Y, Z, HP1, HP2, P1, P2](
+def port[S1, S2, X, Y, Z, HP1, HP2, P1, P2](
     t: Seq[S1, S2, X, Y, Z, HP1, HP2, P1, P2], ctx: int, key: PRNG
-) -> tuple[tuple[tuple[HP1, HP2], tuple[P1, P2]], tuple[S1, S2]]:
+) -> tuple[tuple[HP1, HP2], tuple[P1, P2]]:
     k1, k2 = jax.random.split(key)
-    (hp1, p1), s1 = init(t.first, ctx, PRNG(k1))
-    (hp2, p2), s2 = init(t.second, out(t.first, ctx), PRNG(k2))
-    return (((hp1, hp2), (p1, p2)), (s1, s2))
+    hp1, p1 = port(t.first, ctx, PRNG(k1))
+    hp2, p2 = port(t.second, out(t.first, ctx), PRNG(k2))
+    return ((hp1, hp2), (p1, p2))
 
 
 @overload
-def init[S, X, HP, P](
+def port[S, X, HP, P](
     t: Sup[S, X, HP, P], ctx: int, key: PRNG
-) -> tuple[
-    tuple[tuple[tuple[HP, Unit], Unit], tuple[tuple[P, Unit], Unit]],
-    tuple[tuple[S, Unit], Unit],
-]:
-    k1, k2 = jax.random.split(key)
-    (hp_a, p_a), s_a = init(t.arch, ctx, PRNG(k1))
-    (hp_l, p_l), s_l = init(t.loss, out(t.arch, ctx), PRNG(k2))
-    return ((((hp_a, Unit()), hp_l), ((p_a, Unit()), p_l)), ((s_a, Unit()), s_l))
+) -> tuple[tuple[tuple[HP, Unit], Unit], tuple[tuple[P, Unit], Unit]]:
+    hp_a, p_a = port(t.arch, ctx, key)
+    return (((hp_a, Unit()), Unit()), ((p_a, Unit()), Unit()))
 
 
 @overload
-def init[S, X, HP, P, H, HPO, HPV, SV, XV, PV](
+def port[S, X, HP, P, H, HPO, HPV, SV, XV, PV](
     t: Meta[S, X, HP, P, H, HPO, HPV, SV, XV, PV], ctx: int, key: PRNG
-) -> tuple[
-    tuple[tuple[tuple[HPO, Unit], HPV], tuple[tuple[tuple[HP, H], Unit], PV]],
-    tuple[tuple[tuple[S, tuple[ArrayTree, P]], Unit], SV],
-]:
+) -> tuple[tuple[tuple[HPO, Unit], HPV], tuple[tuple[tuple[HP, H], Unit], PV]]:
     k1, k2, k3 = jax.random.split(key, 3)
-    (hp, p), s = init(t.below, ctx, PRNG(k1))
-    (hp_o, h), opt_st = init(t.opt, p, PRNG(k2))
-    (hpv, pv), s_v = val_init(t.val, s, ctx, PRNG(k3))
-    return ((((hp_o, Unit()), hpv), (((hp, h), Unit()), pv)), (((s, (opt_st, p)), Unit()), s_v))
+    hp, _ = port(t.below, ctx, PRNG(k1))
+    hp_o, h = port(t.opt, ctx, PRNG(k2))
+    hpv, pv = val_port(t.val, ctx, PRNG(k3))
+    return (((hp_o, Unit()), hpv), (((hp, h), Unit()), pv))
 
 
 @overload
-def init[S, X, Y, HP, P](t: Scan[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[tuple[HP, P], S]:
-    return init(t.below, ctx, key)
+def port[S, X, Y, HP, P](t: Scan[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[HP, P]:
+    return port(t.below, ctx, key)
 
 
 @overload
-def init[S, X, Y, HP, P](t: BatchData[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[tuple[HP, P], S]:
-    k0, kb = jax.random.split(key)
-    (hp, p), _ = init(t.below, ctx, PRNG(k0))
-    _, s = eqx.filter_vmap(lambda k: init(t.below, ctx, PRNG(k)))(jax.random.split(kb, t.n))
-    return ((hp, p), s)
+def port[S, X, Y, HP, P](t: BatchData[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[HP, P]:
+    return port(t.below, ctx, key)
 
 
 @overload
-def init[S, X, Y, HP, P](t: BatchPop[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[tuple[HP, P], S]:
-    return eqx.filter_vmap(lambda k: init(t.below, ctx, PRNG(k)))(jax.random.split(key, t.n))
+def port[S, X, Y, HP, P](t: BatchPop[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[HP, P]:
+    return eqx.filter_vmap(lambda k: port(t.below, ctx, PRNG(k)))(jax.random.split(key, t.n))
 
 
 @overload
-def init[S, X, Y, HP, P, C](t: Term[S, X, Y, HP, P], ctx: C, key: PRNG) -> tuple[tuple[HP, P], S]:
+def port[S, X, Y, HP, P](t: Term[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[HP, P]:
     raise NotImplementedError
 
 
 @dispatch
-def init[S, X, Y, HP, P, C](t: Term[S, X, Y, HP, P], ctx: C, key: PRNG) -> tuple[tuple[HP, P], S]:
+def port[S, X, Y, HP, P](t: Term[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[HP, P]:
     raise NotImplementedError
+
+
+@overload
+def val_state[S, X, Y, HP, P](v: SameModel[S, X, Y, HP, P], s: S, ctx: int, key: PRNG) -> S:
+    return s
+
+
+@overload
+def val_state[S, X, Y, HP, P, SV, XV, HPV, PV](
+    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], s: S, ctx: int, key: PRNG
+) -> SV:
+    raise NotImplementedError
+
+
+@dispatch
+def val_state[S, X, Y, HP, P, SV, XV, HPV, PV](
+    v: Validator[S, X, Y, HP, P, SV, XV, HPV, PV], s: S, ctx: int, key: PRNG
+) -> SV:
+    raise NotImplementedError
+
+
+@overload
+def state(t: Linear, hp_p: tuple[Unit, eqx.nn.Linear], ctx: int, key: PRNG) -> Unit:
+    return Unit()
+
+
+@overload
+def state(t: Bias, hp_p: tuple[Unit, jax.Array], ctx: int, key: PRNG) -> Unit:
+    return Unit()
+
+
+@overload
+def state(t: Activation, hp_p: tuple[Unit, Unit], ctx: int, key: PRNG) -> Unit:
+    return Unit()
+
+
+@overload
+def state(t: Loss, hp_p: tuple[Unit, Unit], ctx: int, key: PRNG) -> Unit:
+    return Unit()
+
+
+@overload
+def state[HPA, PA](t: Rnn[HPA, PA], hp_p: tuple[HPA, tuple[PA, eqx.nn.Linear]], ctx: int, key: PRNG) -> jax.Array:
+    return initializer(t.h0)(key, (t.n,))
+
+
+@overload
+def state[P](t: Sgd[P], hp_p: tuple[Unit, jax.Array], ctx: P, key: PRNG) -> ArrayTree:
+    _, lr = hp_p
+    return optax.sgd(lr).init(cast(optax.Params, ctx))
+
+
+@overload
+def state[S1, S2, X, Y, Z, HP1, HP2, P1, P2](
+    t: Seq[S1, S2, X, Y, Z, HP1, HP2, P1, P2],
+    hp_p: tuple[tuple[HP1, HP2], tuple[P1, P2]],
+    ctx: int,
+    key: PRNG,
+) -> tuple[S1, S2]:
+    (hp1, hp2), (p1, p2) = hp_p
+    k1, k2 = jax.random.split(key)
+    return (state(t.first, (hp1, p1), ctx, PRNG(k1)), state(t.second, (hp2, p2), out(t.first, ctx), PRNG(k2)))
+
+
+@overload
+def state[S, X, HP, P](
+    t: Sup[S, X, HP, P],
+    hp_p: tuple[tuple[tuple[HP, Unit], Unit], tuple[tuple[P, Unit], Unit]],
+    ctx: int,
+    key: PRNG,
+) -> tuple[tuple[S, Unit], Unit]:
+    ((hp_a, _), _), ((p_a, _), _) = hp_p
+    return ((state(t.arch, (hp_a, p_a), ctx, key), Unit()), Unit())
+
+
+@overload
+def state[S, X, HP, P, H, HPO, HPV, SV, XV, PV](
+    t: Meta[S, X, HP, P, H, HPO, HPV, SV, XV, PV],
+    hp_p: tuple[tuple[tuple[HPO, Unit], HPV], tuple[tuple[tuple[HP, H], Unit], PV]],
+    ctx: int,
+    key: PRNG,
+) -> tuple[tuple[tuple[S, tuple[ArrayTree, P]], Unit], SV]:
+    _, (((hp, h), _), _) = hp_p
+    k1, k2, k3 = jax.random.split(key, 3)
+    _, p_b = port(t.below, ctx, PRNG(k1))
+    s = state(t.below, (hp, p_b), ctx, PRNG(k2))
+    opt_st = state(t.opt, (Unit(), h), p_b, PRNG(k3))
+    return (((s, (opt_st, p_b)), Unit()), val_state(t.val, s, ctx, key))
+
+
+@overload
+def state[S, X, Y, HP, P](t: Scan[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG) -> S:
+    return state(t.below, hp_p, ctx, key)
+
+
+@overload
+def state[S, X, Y, HP, P](t: BatchData[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG) -> S:
+    return eqx.filter_vmap(lambda k: state(t.below, hp_p, ctx, PRNG(k)))(jax.random.split(key, t.n))
+
+
+@overload
+def state[S, X, Y, HP, P](t: BatchPop[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG) -> S:
+    hp, p = hp_p
+    return eqx.filter_vmap(lambda h, q, k: state(t.below, (h, q), ctx, PRNG(k)))(hp, p, jax.random.split(key, t.n))
+
+
+@overload
+def state[S, X, Y, HP, P, C](t: Term[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: C, key: PRNG) -> S:
+    raise NotImplementedError
+
+
+@dispatch
+def state[S, X, Y, HP, P, C](t: Term[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: C, key: PRNG) -> S:
+    raise NotImplementedError
+
+
+def flat_state[S, X, Y, HP, P](
+    t: Term[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG
+) -> Callable[[jax.Array], jax.Array]:
+    hp, p = hp_p
+    diff, static = eqx.partition(p, eqx.is_inexact_array)
+    _, unravel = jax.flatten_util.ravel_pytree(diff)
+
+    def f(p_flat: jax.Array) -> jax.Array:
+        s = state(t, (hp, eqx.combine(unravel(p_flat), static)), ctx, key)
+        s_flat, _ = jax.flatten_util.ravel_pytree(eqx.filter(s, eqx.is_inexact_array))
+        return s_flat
+
+    return f
+
+
+def influence[S, X, Y, HP, P](t: Term[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG) -> JACOBIAN:
+    _, p = hp_p
+    p_flat, _ = jax.flatten_util.ravel_pytree(eqx.filter(p, eqx.is_inexact_array))
+    f = flat_state(t, hp_p, ctx, key)
+    (n_s,) = jax.eval_shape(f, p_flat).shape
+    return JACOBIAN(jax.jacfwd(f)(p_flat) if n_s > p_flat.size else jax.jacrev(f)(p_flat))
+
+
+def rank_one[S, X, Y, HP, P](
+    t: Term[S, X, Y, HP, P], hp_p: tuple[HP, P], ctx: int, key: PRNG, nu: jax.Array
+) -> jax.Array:
+    _, p = hp_p
+    p_flat, _ = jax.flatten_util.ravel_pytree(eqx.filter(p, eqx.is_inexact_array))
+    _, vjp = jax.vjp(flat_state(t, hp_p, ctx, key), p_flat)
+    (d_p,) = vjp(nu)
+    return d_p
+
+
+def init[S, X, Y, HP, P](t: Term[S, X, Y, HP, P], ctx: int, key: PRNG) -> tuple[tuple[HP, P], S]:
+    k1, k2 = jax.random.split(key)
+    hp_p = port(t, ctx, PRNG(k1))
+    return hp_p, state(t, hp_p, ctx, PRNG(k2))

036080b5e6fc9b42a3d5e9e83393e1961a7bb9f3
thewillyP <williamsleepark@gmail.com>
2026-09-14 15:45:01 -0400

    made loggers better

--- src/meta_learn_lib/loggers.py
+++ src/meta_learn_lib/loggers.py
@@ -8,54 +8,68 @@ from meta_learn_lib.experiment import (
 )
 from meta_learn_lib.logger import ClearMLLogger, ConsoleLogger, HDF5Logger, Logger, MatplotlibLogger, SQLiteLogger
 
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import overload
+from typing import Protocol, overload, runtime_checkable
 import clearml
 from plum import dispatch
 
 
-@dataclass(frozen=True)
-class Run:
-    log_dir: str
-    task_id: str
-    checkpoint_every: int
+@runtime_checkable
+class NoDeps(Protocol): ...
+
+
+@runtime_checkable
+class HasRunId(Protocol):
+    def run_id(self) -> str: ...
+
+
+@runtime_checkable
+class HasClearML(Protocol):
+    def task(self) -> clearml.Task: ...
 
 
 @dataclass(frozen=True)
-class ClearMLRun(Run):
-    task: clearml.Task
+class LocalRun:
+    run_id: Callable[[], str]
+
+
+@dataclass(frozen=True)
+class ClearMLRun:
+    run_id: Callable[[], str]
+    task: Callable[[], clearml.Task]
 
 
 @overload
-def logger(t: ConsoleLoggerConfig, run: Run) -> Logger:
+def logger(t: ConsoleLoggerConfig, run: NoDeps) -> Logger:
     return ConsoleLogger()
 
 
 @overload
-def logger(t: MatplotlibLoggerConfig, run: Run) -> Logger:
+def logger(t: MatplotlibLoggerConfig, run: NoDeps) -> Logger:
     return MatplotlibLogger(t.save_dir)
 
 
 @overload
-def logger(t: SQLiteLoggerConfig, run: Run) -> Logger:
-    return SQLiteLogger(run.log_dir, run.task_id, run.checkpoint_every)
+def logger(t: SQLiteLoggerConfig, run: HasRunId) -> Logger:
+    return SQLiteLogger(t.log_dir, run.run_id(), t.checkpoint_every)
 
 
 @overload
-def logger(t: HDF5LoggerConfig, run: Run) -> Logger:
-    return HDF5Logger(run.log_dir, run.task_id, run.checkpoint_every)
+def logger(t: HDF5LoggerConfig, run: HasRunId) -> Logger:
+    return HDF5Logger(t.log_dir, run.run_id(), t.checkpoint_every)
 
 
 @overload
-def logger(t: ClearMLLoggerConfig, run: ClearMLRun) -> Logger:
-    return ClearMLLogger(run.task)
+def logger(t: ClearMLLoggerConfig, run: HasClearML) -> Logger:
+    return ClearMLLogger(run.task())
 
 
 @overload
-def logger(t: LoggerConfig, run: Run) -> Logger:
+def logger(t: LoggerConfig, run: NoDeps) -> Logger:
     raise NotImplementedError
 
 
 @dispatch
-def logger(t: LoggerConfig, run: Run) -> Logger:
+def logger(t: LoggerConfig, run: NoDeps) -> Logger:
     raise NotImplementedError
